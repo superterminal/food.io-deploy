db43c40b0a733ebb10b030bebc1fa47e5752da87
superterminal <trendemil@gmail.com>
2020-01-01 14:22:55 +0200

    updated

--- server-side/client-side/src/App/PageNotExists/PageNotExists.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './PageNotExists.css';
-
-function PageNotExists() {
-    return <h1>Page does not exist</h1>
-}
-
-export default PageNotExists;
--- server-side/utils/index.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('./jwt');
-const auth = require('./auth');
-const withAuth = require('./withAuth');
-
-module.exports = {
-    jwt,
-    auth,
-    withAuth
-};
